87842063e6d7e05c5ae11b08f0c8edf242a1149c
APIVerve Team <hello@apiverve.com>
2024-05-14 15:21:36 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,67 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class code
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("size")]
+    public int size { get; set; }
+
+}
+
+public class card
+{
+    [JsonProperty("niceType")]
+    public string niceType { get; set; }
+
+    [JsonProperty("type")]
+    public string type { get; set; }
+
+    [JsonProperty("patterns")]
+    public int[] patterns { get; set; }
+
+    [JsonProperty("gaps")]
+    public int[] gaps { get; set; }
+
+    [JsonProperty("lengths")]
+    public int[] lengths { get; set; }
+
+    [JsonProperty("code")]
+    public code code { get; set; }
+
+    [JsonProperty("matchStrength")]
+    public int matchStrength { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("card")]
+    public card card { get; set; }
+
+    [JsonProperty("cardNumber")]
+    public string cardNumber { get; set; }
+
+    [JsonProperty("isValid")]
+    public bool isValid { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
